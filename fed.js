c97bd8bfcd2511cd543da6a87b9d17b102cdae21
fedy14 <jeanpierrefedy@gmail.com>
2024-11-25 16:11:49 -0400

    first commit

---
+++ fed.js
@@ -0,0 +1,56 @@
+// Calculate and log the sum
+let a = 4;
+let b = 10;
+let sum = a + b;
+console.log(a);  
+console.log(b);  
+console.log(sum);       
+
+// Calculate and log the difference
+let difference = b - a;
+console.log(difference); 
+
+// Calculate and log the product 
+let product = b * a;
+console.log(product);
+
+// Calculate and log the division 
+let division = b / a;
+console.log(division);
+
+
+// Strings exercise
+const firstName = "Fedy";
+const lastName = "Jean Pierre";
+const fullname = firstName + " " + lastName;
+console.log(firstName);
+console.log(lastName);
+console.log(fullname);
+
+
+// Objects exercise
+let person = {
+    name: "Fedy",
+    age: 33,
+    hobby: "musique"
+};
+console.log(person.name);
+console.log(person.age);
+console.log(person.hobby);
+
+
+// Arrays exercise
+let food = ["Rice", "Spaghetti", "Pica pollo", "Stew"];
+console.log(food);
+console.log(food[0]);
+console.log(food[3]);
+
+// Bonus task
+const car = {
+    brand: "Tesla",
+    model: "Model S",
+    year: 2023,
+    features: ["Autopilot", "Electric Engine", "Touchscreen Dashboard"]
+};
+console.log(car);
+console.log(car.features[0]); 
